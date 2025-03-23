ff61b202d31b3cd52fbd8beec1a34a07b85eff33
Raherimalalanantenaina <168984775+Raherimalalanantenaina@users.noreply.github.com>
2025-03-23 13:21:40 +0100

    budget
    budjet

--- src/main/java/site/easy/to/build/crm/entity/Budget.java
+++ src/main/java/site/easy/to/build/crm/entity/Budget.java
@@ -1,54 +1,61 @@
 package site.easy.to.build.crm.entity;
 
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "lead_expense")
-public class LeadExpense {
+@Table(name = "budget")
+public class Budget {
     @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private int budgetId;
 
-    @Column(name = "amount", nullable = false, precision = 18, scale = 2)
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
     @NotNull(message = "Amount is required")
     @Digits(integer = 10, fraction = 2, message = "Amount must be a valid number with up to 2 decimal places")
     @DecimalMin(value = "0.00", inclusive = true, message = "Amount must be greater than or equal to 0.00")
     @DecimalMax(value = "9999999.99", inclusive = true, message = "Amount must be less than or equal to 9999999.99")
     private BigDecimal amount;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "created_at")
+    @NotNull(message = "Date is required")
+    private LocalDate createdAt;
 
     @ManyToOne
-    @JoinColumn(name = "trigger_lead_histo_id", nullable = false)
-    private TriggerLeadHisto triggerLeadHisto;
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
-    public LeadExpense() {
+    public Budget() {
     }
 
-    public LeadExpense(
+    public Budget(
             @NotNull(message = "Amount is required") @Digits(integer = 10, fraction = 2, message = "Amount must be a valid number with up to 2 decimal places") @DecimalMin(value = "0.00", inclusive = true, message = "Amount must be greater than or equal to 0.00") @DecimalMax(value = "9999999.99", inclusive = true, message = "Amount must be less than or equal to 9999999.99") BigDecimal amount,
-            LocalDateTime createdAt, TriggerLeadHisto triggerLeadHisto) {
+            LocalDate createdAt, Customer customer) {
         this.amount = amount;
         this.createdAt = createdAt;
-        this.triggerLeadHisto = triggerLeadHisto;
+        this.customer = customer;
     }
 
-    // Getters et Setters
-    public Integer getId() {
-        return id;
+    public int getBudgetId() {
+        return budgetId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setBudgetId(int budgetId) {
+        this.budgetId = budgetId;
     }
 
     public BigDecimal getAmount() {
@@ -59,19 +66,19 @@ public class LeadExpense {
         this.amount = amount;
     }
 
-    public LocalDateTime getCreatedAt() {
+    public LocalDate getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
+    public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
     }
 
-    public TriggerLeadHisto getTriggerLeadHisto() {
-        return triggerLeadHisto;
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void setTriggerLeadHisto(TriggerLeadHisto triggerLeadHisto) {
-        this.triggerLeadHisto = triggerLeadHisto;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
